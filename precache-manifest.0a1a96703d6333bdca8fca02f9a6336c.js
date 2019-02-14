c18527d5d6e57abedafdfb39edd48df5ddeaee9a
arseniyT <30984464+arseniyT@users.noreply.github.com>
2019-02-15 00:19:24 +0300

    Updates

---
+++ precache-manifest.0a1a96703d6333bdca8fca02f9a6336c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0342ace2c4ed101d829c",
+    "url": "/2nd_hand/static/css/main.54e53644.chunk.css"
+  },
+  {
+    "revision": "0342ace2c4ed101d829c",
+    "url": "/2nd_hand/static/js/main.0342ace2.chunk.js"
+  },
+  {
+    "revision": "36bda717f6d37b2e9e07",
+    "url": "/2nd_hand/static/js/1.36bda717.chunk.js"
+  },
+  {
+    "revision": "d29f33a7fb952b814e32",
+    "url": "/2nd_hand/static/js/runtime~main.d29f33a7.js"
+  },
+  {
+    "revision": "dc826ffec1395b89bb7d4827982376fe",
+    "url": "/2nd_hand/index.html"
+  }
+];
